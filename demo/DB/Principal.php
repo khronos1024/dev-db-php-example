1331cede138059fe074690d2b986658c9f013616
khronos1024 <jeanvesu@gmail.com>
2020-01-21 05:04:16 -0500

    Charge "insert into" on the method

--- demo/DB/Principal.php
+++ demo/DB/Principal.php
@@ -2,5 +2,15 @@
     //call conextion 
     Include("Conexion.php");
     $conn = conectar();
-    echo "Conextion successfully";
+    echo "Connection successfully";
+    
+ 	$sql = "INSERT INTO user (id, user_name, user_password)
+	VALUES ('', 'Hanzo', '12345')";
+
+	if ($conn-> query($sql) === TRUE) {
+	    echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn->close();
 ?>
